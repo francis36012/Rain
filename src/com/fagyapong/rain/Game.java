441a59dc4063032ab8d15a06c67b981f12991cf5
Francis Agyapong <francisagyapong2@gmail.com>
2014-02-15 15:25:25 -0700

    ep2 - intro to threads

--- src/com/fagyapong/rain/Game.java
+++ src/com/fagyapong/rain/Game.java
@@ -1,10 +1,29 @@
 package com.fagyapong.rain;
 
-public class Game {
-	
+public class Game implements Runnable{
 	
 	public static int width = 300;
 	public static int height = width / 16 * 9;
 	public static int scale = 3;
 	
+	public Thread thread;
+	
+	public synchronized void start() {
+		
+		thread = new Thread(this, "Display");
+		thread.start();
+	}
+	
+	public synchronized void stop() {
+		
+		try {
+			thread.join();
+		}
+		catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void run() {
+	}
 }
